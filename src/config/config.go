17b4fc08ee444a11683d18a90e42e5737882d51c
Bashsoft707 <alausabashir@gmail.com>
2026-05-13 17:23:15 +0100

    Build config loading

---
+++ src/config/config.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Config struct {
+	GeminAPIKey string `json:"gemini_api_key"`
+	ArchivePath string `json:"archive_path"`
+	OutputPath string `json:"output_path"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	// Read file
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create config variable
+	var config Config
+
+	// Convert JSOn -> struct
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
--- src/main.go
+++ src/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"github.com/Bashsoft707/tweet-audit/src/config"
+
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Tweet Audit Started")
+
+	cfg, err := config.LoadConfig("config.json")
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Config Loaded Successfully")
+	fmt.Println("Archive path:", cfg.ArchivePath)
 }
