4f54138d202f2d246eab27b7f1b4b1449d9733c3
Bashsoft707 <alausabashir@gmail.com>
2026-06-07 01:51:17 +0100

    Analyse tweet with gemini

--- src/main.go
+++ src/main.go
@@ -4,9 +4,13 @@ import (
 	"github.com/Bashsoft707/tweet-audit/src/analyzer"
 	"github.com/Bashsoft707/tweet-audit/src/archive"
 	"github.com/Bashsoft707/tweet-audit/src/config"
+	"github.com/Bashsoft707/tweet-audit/src/gemini"
+	"github.com/Bashsoft707/tweet-audit/src/models"
 	"github.com/Bashsoft707/tweet-audit/src/report"
 
 	"fmt"
+	"strings"
+	"time"
 )
 
 func main() {
@@ -31,25 +35,54 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	
-	// Prints all Tweets
-	if len(tweets) > 0 {
-		for _, tweet := range tweets {
-			fmt.Printf("Tweet ID: %s\nText: %s\nCreated At: %s\n\n", tweet.ID, tweet.FullText, tweet.CreatedAt)
-		}
-	}
 
 	flaggedTweets := analyzer.AnalyseTweets(tweets, cfg)
+	flaggedIDs := make(map[string]bool)
 
 	for _, tweet := range flaggedTweets {
-		fmt.Printf(
-			"Tweet: %s\nReason: %s\nURL: %s\n\n",
-			tweet.Text,
-			tweet.Reason,
-			tweet.URL,
-		)
+		flaggedIDs[tweet.TweetID] = true
+		// fmt.Printf(
+		// 	"Tweet: %s\nReason: %s\nURL: %s\n\n",
+		// 	tweet.Text,
+		// 	tweet.Reason,
+		// 	tweet.URL,
+		// )
 	}
 
+
+	baseURL := "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent"
+
+	count := 0
+	for _, tweet := range tweets {
+		// test first 20 tweets with Gemini
+		if count >= 20 {
+			break
+		}
+		if flaggedIDs[tweet.ID] {
+			continue
+		}
+
+		result, err := gemini.AnalyseTweet(tweet, cfg.GeminiAPIKey, baseURL, cfg)
+		count++
+		time.Sleep(4 * time.Second)
+
+		if err != nil {
+			fmt.Printf("Warning: failed to analyse tweet %s: %v\n", tweet.ID, err)
+    		continue
+		}
+		if strings.HasPrefix(result, "FLAGGED") {
+			fmt.Printf("Tweet ID: %s flagged by Gemini with reason: %s\n", tweet.ID, result)
+			flaggedTweet := models.FlaggedTweet{
+				TweetID: tweet.ID,
+				Text: tweet.FullText,
+				Reason: "Flagged by Gemini: " + result,
+				URL: "https://x.com/" + cfg.Username + "/status/" + tweet.ID,
+				Deleted: false,
+			}
+			flaggedTweets = append(flaggedTweets, flaggedTweet)
+		}
+	}
+	
 	err = report.WriteCSV(cfg.OutputPath, flaggedTweets)
 
 	if err != nil {
